5ea39c4412ca3d200d87d47c97632d747be284b7
FELIPE BRITO ALEXANDRE FAUSTINO <49381762899@imaccommand001.local>
2025-11-26 16:50:54 -0300

    subindo

---
+++ Estrutura de Repetição/for.js
@@ -0,0 +1,27 @@
+// sintaxe = escrita do código
+
+// for - laço de repetição = loop
+// um laço de repetição tem voltas 
+
+// laço contato - quando sabemos quantas voltas 
+// laço condicional - irá fazer as voltas se a condição for verdadeira
+
+
+
+
+// a variavel i vale 0 ou seja essa  essa variavel armazena as voltas 
+// a condicao verifica o numero que o logo está
+ for(let i = 0; i <= 9; i = i + 1){
+    console.log("A volta que for esta e:" + i);
+
+ }
+
+ // utilize o for para exibir do 1 ao 10
+ for( let i = 1; i <= 10; i++){
+    console
+ }
+
+
+ for(let i = 1; i <= 10; i = i + 1){
+console.log("a volta que for esta e: " + i)
+}
