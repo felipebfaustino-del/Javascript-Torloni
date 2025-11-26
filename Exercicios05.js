e8feaff6ac25981a5261460e90602dbed7976438
FELIPE BRITO ALEXANDRE FAUSTINO <49381762899@imaccommand001.local>
2025-11-26 14:50:36 -0300

    Exercicio Finalizado

---
+++ Exercicios05.js
@@ -0,0 +1,14 @@
+let n1 = Number ( prompt(" Digite o primeiro número da sua sequência"));
+let n2 = Number ( prompt(" Digite o segundo número da sua sequência"));
+let n3 = Number ( prompt(" Digite o terceiro número da sua sequência"));
+
+
+if ( n1 < n2 && n2 < n3 ) {
+     alert(" Os números estão em ordem crescente");
+}     
+     if (n1 > n2 && n2 > n3 ){
+          alert("esse numero esta em ordem decrecente")
+}
+     else{
+          alert("esse numero esta em ordem aleatoria ")
+     }
